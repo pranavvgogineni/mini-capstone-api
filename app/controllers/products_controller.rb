281f933753d9b68aba9acc6f315cae51c1dfcf12
Pranav Gogineni <pranavgv@gmail.com>
2023-07-06 20:09:43 -0500

    Added a create route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     products = Product.find_by(id: params["id"])
     render json: products.as_json
   end
+
+  def create
+    product = Product.create(
+      name: "Video game",
+      price: 60,
+      image_url: "insert",
+      description: "Game meant for fun played on console",
+    )
+    render json: product.as_json
+  end
 end
