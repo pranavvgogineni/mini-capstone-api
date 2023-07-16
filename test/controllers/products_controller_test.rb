bbcfb59619e64756ec5c969491353d227bb8c514
Pranav Gogineni <pranavgv@gmail.com>
2023-07-16 11:01:26 -0500

    updates tests and fixtures

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,12 +14,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "description", "image_url", "price", "is_discounted?", "tax", "total"], data.keys
+    assert_equal ["id", "name", "description", "price", "is_discounted?", "tax", "total", "supplier", "images"], data.keys
   end
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test", price: 10, image_url: "test.jpg", description: "test description" }
+      post "/products.json", params: { name: "test", price: 10, supplier_id: Supplier.first.id, description: "test description" }
       assert_response 200
     end
 
@@ -35,7 +35,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
     assert_equal product.price.to_s, data["price"]
-    assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
 
     patch "/products/#{product.id}.json", params: { name: "" }
