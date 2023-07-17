a376ce2569bee9b39da3a80d8ee9c24d7787e65d
Pranav Gogineni <pranavgv@gmail.com>
2023-07-17 18:51:54 -0500

    Add users create action (signup)

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -6,4 +6,5 @@ json.is_discounted? product.is_discounted?
 json.tax product.tax
 json.total product.total
 json.supplier product.supplier
+json.supplier_id product.supplier_id
 json.images product.images
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,7 +14,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "description", "price", "is_discounted?", "tax", "total", "supplier", "images"], data.keys
+    assert_equal ["id", "name", "description", "price", "is_discounted?", "tax", "total", "supplier", "supplier_id", "images"], data.keys
   end
 
   test "create" do
@@ -31,11 +31,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     product = Product.first
     patch "/products/#{product.id}.json", params: { name: "Updated name" }
     assert_response 200
-
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
     assert_equal product.price.to_s, data["price"]
     assert_equal product.description, data["description"]
+    assert_equal product.supplier_id, data["supplier_id"]
 
     patch "/products/#{product.id}.json", params: { name: "" }
     assert_response 422
