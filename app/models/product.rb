534c5aafb8cc69612a5b50c8c4519873191708db
Pranav Gogineni <pranavgv@gmail.com>
2023-07-18 21:02:05 -0500

    Adds a create and show route for the order controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
 
   belongs_to :supplier
   has_many :images
+  has_many :orders
 
   def supplier
     Supplier.find_by(id: self.supplier_id)
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   delete "/products/:id" => "products#destroy"
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+  post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
 end
