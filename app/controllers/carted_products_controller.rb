01e496becffee54d385a66ed8d1fcda2a8ab2205
Pranav Gogineni <pranavgv@gmail.com>
2023-07-23 15:29:46 -0500

    Made a create action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,15 @@
 class CartedProductsController < ApplicationController
+  def create
+    @carted_product = CartedProduct.create!(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: "carted",
+    )
+    if @carted_product
+      render :show
+    else
+      render json: { errors: @carted_product.errors.full_messages }, status: 422
+    end
+  end
 end
